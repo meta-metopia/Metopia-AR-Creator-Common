ba08b9f18e1e77d5cc1593e929b6c9c10251cc40
sirily11 <sirily1997@gmail.com>
2022-11-28 01:14:33 +0800

    fix: add ar viewing settings

---
+++ Sources/MetopiaARCreatorCommon/Types/Settings.swift
@@ -0,0 +1,34 @@
+//
+//  Settings 2.swift
+//  
+//
+//  Created by Qiwei Li on 11/28/22.
+//
+
+import Foundation
+
+/**
+ List of ar settings
+ */
+public enum ARSettings: String, CaseIterable {
+  case personSegmentation = "Person segmentation"
+  case objectOcclusion = "Object occlusion"
+  case ultraHD = "4k Resolution"
+  case debug = "Debug Mode"
+  
+  /**
+   Icon for settings
+   */
+  var icon: String {
+    switch self {
+      case .personSegmentation:
+        return "person.crop.rectangle.fill"
+      case .objectOcclusion:
+        return "square.3.stack.3d.middle.filled"
+      case .ultraHD:
+        return "4k.tv.fill"
+      case .debug:
+        return "eyeglasses"
+    }
+  }
+}
